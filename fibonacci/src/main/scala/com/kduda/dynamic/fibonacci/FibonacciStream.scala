84b12c91add12e9cce9c454aba2c808ee860cc87
Kamil Duda <kamilduda01@gmail.com>
2021-09-12 11:50:20 +0200

    Use LazyList instead of deprecated Stream

--- fibonacci/src/main/scala/com/kduda/dynamic/fibonacci/FibonacciStream.scala
+++ fibonacci/src/main/scala/com/kduda/dynamic/fibonacci/FibonacciStream.scala
@@ -3,7 +3,7 @@ package com.kduda.dynamic.fibonacci
 object FibonacciStream {
   def apply(n: Int): BigInt = {
 
-    def _fibonacci(nMinusTwo: BigInt, nMinusOne: BigInt): Stream[BigInt] =
+    def _fibonacci(nMinusTwo: BigInt, nMinusOne: BigInt): LazyList[BigInt] =
       nMinusTwo #:: _fibonacci(nMinusOne, nMinusTwo + nMinusOne)
 
     _fibonacci(0, 1).apply(n)
